f33734ab79184184977ee4d740316059f8aa4787
Epharra Mendoza <mendoza@hoare7.cs.umsl.edu>
2020-10-16 13:02:26 -0500

    Free memory, spawn children, sending to palin

--- master.c
+++ master.c
@@ -38,6 +38,7 @@
 //Shared memory global
 typedef struct{
 	char words[64][64]; 		//Assuming 20 words in a file with length 256 max
+	unsigned int children_count;	//The amount of child processes
 
 	//memory attributes
 	pid_t pgid;			//Group pid
@@ -48,14 +49,21 @@ typedef struct{
 
 //Memory globals
 shared_memory_data* shm_data_ptr; 	//shared memory pointer to shared memory struct
-int shmid;				//shared memory segment id
+unsigned int shmid;			//shared memory segment id
+int spawn_count;			//Tracks the amount of times a spawn was tried
+int curr_processes_running = 0;		//Tracks the current amount of processes running
 
 
 //Default arguments if no user input
-int max_total_cp = 4;
-int concurr_children = 2; 
-int forced_time_quit = 100;
+int max_total_cp = 4;			//Maximum total amount of child processes to exist
+int max_concurr_children = 2; 		//Number of children allowed to exist at the same time
+int forced_time_quit = 100;		//Time in seconds when the process will termiante
 
+
+//Prototypes
+void run_child(int spawn_count);	//Creates child and sends to palin
+void free_shared_mem();			//Frees shared memory
+void free_sem();			//Frees semephore
 
 
 
@@ -68,6 +76,8 @@ int forced_time_quit = 100;
 
 
 int main(int argc, char* argv[]){ 
+
+/*---------------------Create shared memory key----------------------------------*/
 	
 	//memory variables
 	//Use ftok to convert pathname and id value 'a' to System V IPC key
@@ -78,6 +88,8 @@ int main(int argc, char* argv[]){
 		return 1;
 	}	
 
+
+
 /*----------------------Capturing User Input---------------------------------------*/
 	//User input option char
 	int opt; 
@@ -85,6 +97,7 @@ int main(int argc, char* argv[]){
 	//Use getopt function to parse arguments 
 	while((opt = getopt(argc, argv, "hn:s:t:")) !=-1){
 		switch(opt){ 
+
 			//Help option 
 			case 'h': 
 				printf("\n master should take in several command line options as follows:\n");
@@ -96,6 +109,7 @@ int main(int argc, char* argv[]){
 				printf("\n -t time	The time in seconds after which the process will terminate, even if it has not finished. (Default 100)\n");	
 				printf("\n infile	Input file containing strings to be tested.\n");
 				return EXIT_SUCCESS;
+
 			
 			//Indicate the maximum total of child processes master will ever create
 			case 'n':
@@ -111,16 +125,18 @@ int main(int argc, char* argv[]){
 				}
 				break;
 
+
 			//Indicate the number of children allowed to exist in the system at the same time
 			case 's': 
 				//user input replaces default value
-				concurr_children = atoi(optarg);
-				if(concurr_children <= 0){ 
+				max_concurr_children = atoi(optarg);
+				if(max_concurr_children <= 0){ 
 					printf("\nERROR: There must be at least 1 concurrent process\n");
 					return EXIT_FAILURE;
 				}
 			break;
 
+			
 			//The time in seconds after which the process will terminate, even if it has not finished 
 			case 't': 
 				forced_time_quit = atoi(optarg);
@@ -161,14 +177,17 @@ int main(int argc, char* argv[]){
 	}
 
 
+
+
 /*----------------Procedure for creating semaphore------------------------------*/
+
 	//use ftok to convert pathname and id value 'b' to System V IPC key
 	//Separate from shared memory key
 	shm_data_ptr->sem_key = ftok("./master", 'b');
 	//If key generation fails then return with error message 
 	if(shm_data_ptr->sem_key == -1) { 
 		perror("\nERROR: Could not generate SEM key");
-		return 1;
+		exit(1);
 	}
 
 	//Get an id that can be used to access the System V semaphore with the given key
@@ -176,14 +195,20 @@ int main(int argc, char* argv[]){
 	shm_data_ptr->sem_id = semget(shm_data_ptr->sem_key, 1, 0666 | IPC_CREAT | IPC_EXCL);
 	if(shm_data_ptr->sem_id == -1) { 
 		perror("\nERROR: Failed to create semaphore identifier");
-		return 1;
+		exit(1);
 	}  
 
-	
+	//Perform control operation on System V semaphore set identified by semid
+	//Sets the semaphore value. Returns with error message if value not set
+	if(semctl(shm_data_ptr->sem_id, 0, SETVAL, 1) == -1) {
+		perror("\nERROR: Failed to perform semaphore control operations");
+		exit(1);
+	}	
 		
 
 
-/*------------------Procedure for saving data from file------------------------------*/	
+
+/*------------------Procedure for reading & saving data from file------------------------------*/	
 	
 	//Capture input file containing strings to be tested
 	char* fileName = argv[optind];
@@ -214,7 +239,138 @@ int main(int argc, char* argv[]){
 	fclose(fptr);
 	
 
-	printf("\nConcurr children is %d forced time quit %d", concurr_children, forced_time_quit);	
 
+
+/*------------------Set value boundaries for child processes------------------------------*/	
+
+	//The amount of concurrent child processes cannot be greater than the total amount of child processes
+	if (max_concurr_children > max_total_cp){
+	/*If concurrent children is greater than  total amount of child processes, reset value
+	 to max total cp*/
+	max_concurr_children = max_total_cp;		
+	}
+
+	//Max total children cannot exceed total amount of strings given to them to read
+	if(total_strings < max_total_cp) { 
+		max_total_cp = total_strings;
+	}
+
+	//Tell shared memory struct how many children there will be
+	shm_data_ptr->children_count = max_total_cp;
+
+
+
+/*-----------------Procedure for checking requirements before spawning children-------------------------*/	
+
+	//Keep spawning until the spawn number reaches the allowed amount of max total child processes
+	//First check that amount of children allowed to exist at the same time is not being exceeded 
+	while(spawn_count < max_concurr_children){
+		//Increase spawn count
+		spawn_count++;
+		/*Check amount of current processes running does not exceed amount of children allowed
+		to exist at the same time and that we have not reached max total amount of child processes*/
+		if((curr_processes_running  < max_concurr_children) && (spawn_count < max_total_cp)){
+			run_child(spawn_count);
+		}
+		//Requirements to spawn have not been met; do not spawn. Reduce spawn count 
+		else {
+			spawn_count--;
+		}
+	}
+
+	/*If spawn count has exceeded amount of concurrent children, we can still spawn but we need to wait
+	for them to finish*/
+	while(curr_processes_running > 0) { 
+		wait(NULL);
+		/*We have waited for process to end, so current processes running in the 
+		system has decremented*/
+		--curr_processes_running;
+		//Try to spawn now, increase spawn count
+		spawn_count++;
+		/*Check amount of current processes running does not exceed amount of children allowed
+		to exist at the same time and that we have not reached max total amount of child processes*/
+		if((curr_processes_running < max_concurr_children) && (spawn_count < max_total_cp)){
+			run_child(spawn_count);
+		}
+		//Requirements to spawn have not been met; do not spawn. Reduce spawn count 
+		else { 
+			spawn_count--;
+		}
+	}
+
+
+
+/*--------------------------Procedure for releasing memory----------------------------------------------*/
+
+	/*At  this point, the requirements above have not been met which means we have spawned all of
+	the amount of children that we are allowed. Time to free memory*/
+	free_shared_mem();
+	free_sem();
+	
+	//Program ends here
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+/*=============================================================================*/
+/*=============================================================================*/
+/*                            FUNCTIONS                                        */
+/*=============================================================================*/
+/*=============================================================================*/
+
+
+
+/*---------------------------Function for spawning children---------------------------------------------*/
+
+void run_child(int spawn_count){
+	//Spawning increases the amount of current processes running
+	++curr_processes_running;
+	//If fork is successful
+	if(fork() == 0){
+		//The first instance of a child will set the group id
+		if(spawn_count ==1) { 
+			shm_data_ptr->pgid = getpid();
+		}
+		//Group processes together
+		setpgid(0, shm_data_ptr->pgid);
+		
+		//Before passing id to palin, need to convert argument to a string
+		char argument[256];
+		sprintf(argument, "%d", spawn_count + 1);	/*Spawn count +1 because we want id 
+								to start at 1, not 0*/
+		//Send to palin
+		execl("./palin", "palin", argument, (char*) NULL);
+		exit(0);		
+	}
+}
+
+
+/*---------------------------Functions for releasing memory---------------------------------------------*/
+
+void free_shared_mem(){
+	//If shared memory struct has data, we need to release memory
+	if(shm_data_ptr != NULL){
+		//If there was an error detaching memory, return with error message
+		if(shmdt(shm_data_ptr) == -1){
+			perror("\nERROR: Could not detach memory segment");
+			exit(1);
+		}
+	}
+	//If shared memory id exists
+	if(shmid > 0) { 
+		//If there was an error removing shared memory segment, return with error message
+		if(shmctl(shmid, IPC_RMID, NULL) == -1) { 	//Remove ipc identifier
+			perror("\nERROR: Could not remove shared memory segment");
+		}
+	}
+}
+
+
+
